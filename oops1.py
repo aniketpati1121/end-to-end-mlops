358a015b71d69c8da42c0e55cb442ff13c584e5d
aniket <aniket.patil.ai@ghrcem.raisoni.net>
2025-03-28 16:50:28 +0530

    git commit -m

---
+++ oops1.py
@@ -0,0 +1,16 @@
+# initiate a class
+class employee: 
+    # special method/magic method/ dunder method - constructor
+    def __init__(self):
+       self.id = (123)
+       self.salary = 50000
+       self.designation = "SDE"
+
+    def travel (self, destination):
+        print (f"Employee is now travelling to {destination}")
+
+# CREATE an obj/instance of the class
+
+sam = employee()
+# print (sam.id)
+sam.travel("pune")
---
+++ rough.py
@@ -0,0 +1,5 @@
+lst = [1,2,3]
+string = ("Hi")
+my_int = 122
+
+print(type (string))
