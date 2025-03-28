26302fcc3be96212d9b2435bf8c4120a678e908e
aniket <aniket.patil.ai@ghrcem.raisoni.net>
2025-03-28 18:30:37 +0530

    message

--- oops_proj.py
+++ oops_proj.py
@@ -15,7 +15,7 @@ class chatbook:
         if user_input == "1":
             self.signup()
         elif user_input == "2":
-            pass
+            self.signin()
         elif user_input == "3":
             pass
         elif user_input =="4":
@@ -35,6 +35,19 @@ class chatbook:
         print("You have signed up successfully !!")
         print("/n")
         self.menu()
+
+    def signin(self):
+        if self.username=='' and self.password=='':
+            print("Please signup first by pressing 1 in the main menu")
+        else:
+            uname = input("enter your email/username here ->")
+            pwd = input ("enter your password here ->") 
+            if self.username==uname and self.password==pwd:
+              print("you have signed in succesfully !!") 
+            else:
+                print("input correct credentails..")
+        print("\n")
+        self.menu()            
 
 
 obj = chatbook() 
