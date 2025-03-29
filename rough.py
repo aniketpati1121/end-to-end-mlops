f132a38888521fdef8536c9302cf6e385bb6f8f0
aniket <aniket.patil.ai@ghrcem.raisoni.net>
2025-03-29 09:50:18 +0530

    overall project conyext

--- rough.py
+++ rough.py
@@ -1,5 +1,9 @@
-lst = [1,2,3]
-string = ("Hi")
-my_int = 122
+# lst = [1,2,3]
+# string = ("Hi")
+# my_int = 122
 
-print(type (string))
+# print(type (string))
+
+from oops_proj.import.chatbook 
+
+user1 = chatbook{}
